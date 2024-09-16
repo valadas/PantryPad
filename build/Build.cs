bffee28c775303b895c7bc2711dac754626df3f0
Daniel <info@danielvaladas.com>
2024-09-15 23:01:32 -0400

    Adjusted build to publish docker container

--- build/Build.cs
+++ build/Build.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.Docker;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitHub;
 using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.Npm;
 using Nuke.Common.Utilities.Collections;
+using Octokit;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
@@ -20,10 +24,12 @@ using static Nuke.Common.IO.PathConstruction;
 [GitHubActions(
     "build",
     GitHubActionsImage.UbuntuLatest,
-    On = new[] { GitHubActionsTrigger.Push },
+    EnableGitHubToken = true,
+    OnPullRequestBranches = ["main", "develop", "release/*"],
+    OnPushBranches = ["main", "develop", "release/*"],
     InvokedTargets = [nameof(CI)],
-    CacheKeyFiles = [],
-    FetchDepth = 0)]
+    FetchDepth = 0,
+    CacheKeyFiles = [])]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -41,8 +47,10 @@ class Build : NukeBuild
 
     [Solution] readonly Solution Solution;
     [GitVersion] readonly GitVersion GitVersion;
+    [GitRepository] readonly GitRepository GitRepository;
 
-    Project PantryPadProject => Solution.GetProject("PantryPad");
+    Nuke.Common.ProjectModel.Project PantryPadProject => Solution.GetProject("PantryPad");
+    static GitHubActions GitHubActions => GitHubActions.Instance;
 
     // PATHS
     AbsolutePath SourceDirectory => RootDirectory / "PantryPad";
@@ -51,11 +59,11 @@ class Build : NukeBuild
     Target Compile => _ => _
         .Executes(() =>
         {
+            NpmTasks.NpmInstall(s => s
+                .SetProcessWorkingDirectory(WwwRootDirectory));
             NpmTasks.NpmRun(s => s
                 .SetProcessWorkingDirectory(WwwRootDirectory)
                 .SetCommand("build"));
-            NpmTasks.NpmInstall(s => s
-                .SetProcessWorkingDirectory(WwwRootDirectory));
             DotNetTasks.DotNetBuild(s => s
                 .SetProjectFile(PantryPadProject)
                 .SetConfiguration(Configuration));
@@ -81,5 +89,109 @@ class Build : NukeBuild
         });
 
     Target CI => _ => _
-        .DependsOn(DockerBuild);
+        .DependsOn(DockerBuild)
+        .DependsOn(Release);
+
+    Target Release => _ => _
+        .OnlyWhenDynamic(() => GitRepository != null && (GitRepository.IsOnMainOrMasterBranch() || GitRepository.IsOnReleaseBranch()))
+        .OnlyWhenDynamic(() => !string.IsNullOrWhiteSpace(GitHubToken))
+        .DependsOn(DockerBuild)
+        .Executes(async () =>
+        {
+            var credentials = new Credentials(GitHubActions.Token);
+            GitHubTasks.GitHubClient = new GitHubClient(new ProductHeaderValue("Eraware.Dnn.Templates"))
+            {
+                Credentials = credentials,
+            };
+            var (owner, name) = (GitRepository.GetGitHubOwner(), GitRepository.GetGitHubName());
+            var version = GitRepository.IsOnMainOrMasterBranch() ? GitVersion.MajorMinorPatch : GitVersion.FullSemVer;
+            var releaseNotes = GetReleaseNotes();
+            var newRelease = new NewRelease(GitVersion.FullSemVer)
+            {
+                Draft = true,
+                Name = $"v{version}",
+                Body = releaseNotes,
+                TargetCommitish = GitVersion.Sha,
+                Prerelease = GitRepository.IsOnReleaseBranch(),
+            };
+
+            var createdRelease = await GitHubTasks
+                .GitHubClient
+                .Repository
+                .Release
+                .Create(owner, name, newRelease);
+
+            // Publish container
+            DockerTasks.DockerLogin(s => s
+                .SetServer("ghcr.io")
+                .SetUsername(GitHubActions.Token)
+                .SetPassword(GitHubActions.Token)
+                .SetProcessLogger((type, output) =>
+                {
+                    if (output.Contains("ERROR:"))
+                    {
+                        Serilog.Log.Error(output);
+                    }
+                    else
+                    {
+                        Serilog.Log.Information(output);
+                    }
+                }));
+            DockerTasks.DockerPush(s => s
+                .SetName($"ghcr.io/{owner}/pantrypad:{GitVersion.MajorMinorPatch}.{GitVersion.CommitsSinceVersionSource}")
+                .SetProcessLogger((type, output) =>
+                {
+                    if (output.Contains("ERROR:"))
+                    {
+                        Serilog.Log.Error(output);
+                    }
+                    else
+                    {
+                        Serilog.Log.Information(output);
+                    }
+                }));
+        });
+
+    private string GetReleaseNotes()
+    {
+        var owner = GitRepository.GetGitHubOwner();
+        var name = GitRepository.GetGitHubName();
+
+        // Get the milestone
+        var milestone = GitHubTasks.GitHubClient.Issue.Milestone.GetAllForRepository(owner, name).Result.Where(m => m.Title == GitVersion.MajorMinorPatch).FirstOrDefault();
+        if (milestone == null)
+        {
+            Serilog.Log.Warning("Milestone not found for this version");
+            return "No release notes for this version.";
+        }
+
+        // Get the PRs
+        var prRequest = new PullRequestRequest()
+        {
+            State = ItemStateFilter.All
+        };
+        var pullRequests = GitHubTasks.GitHubClient.Repository.PullRequest.GetAllForRepository(owner, name, prRequest).Result.Where(p =>
+            p.Milestone?.Title == milestone.Title &&
+            p.Merged == true &&
+            p.Milestone?.Title == GitVersion.MajorMinorPatch);
+
+        // Build release notes
+        var releaseNotesBuilder = new StringBuilder();
+        releaseNotesBuilder.AppendLine($"# {name} {milestone.Title}")
+            .AppendLine("")
+            .AppendLine($"A total of {pullRequests.Count()} pull requests where merged in this release.").AppendLine();
+
+        foreach (var group in pullRequests.GroupBy(p => p.Labels[0]?.Name, (label, prs) => new { label, prs }))
+        {
+            releaseNotesBuilder.AppendLine($"## {group.label}");
+            foreach (var pr in group.prs)
+            {
+                releaseNotesBuilder.AppendLine($"- #{pr.Number} {pr.Title}. Thanks @{pr.User.Login}");
+            }
+        }
+
+        var result = releaseNotesBuilder.ToString();
+        Serilog.Log.Information(result);
+        return result;
+    }
 }
