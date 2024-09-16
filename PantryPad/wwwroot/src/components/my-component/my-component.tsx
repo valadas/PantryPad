e731517ea38ca57b56d25ab296189c50e8446e7d
Daniel <info@danielvaladas.com>
2024-09-16 02:26:36 -0400

    Removed unused code and dependencies
    Removed unused code and dependencies

--- PantryPad/wwwroot/src/components/my-component/my-component.tsx
+++ PantryPad/wwwroot/src/components/my-component/my-component.tsx
@@ -1,5 +1,4 @@
-import { Component, Prop, h, State } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
@@ -7,21 +6,6 @@ import { format } from '../../utils/utils';
   shadow: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
-
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
-
-  /**
-   * The last name
-   */
-  @Prop() last: string;
-
   @State() message: string;
   @State() date: any;
 
@@ -37,17 +21,13 @@ export class MyComponent {
     });
   }
 
-  private getText(): string {
-    return format("this.first", this.middle, this.last);
-  }
 
   render() {
     return <host>
-      <div>Hello, World! I'm {this.getText()}</div>
+      <div>Hello, World!</div>
       <p>{this.message}</p>
       <p>{this.date}</p>
-      <p>Does this work d!!!?</p>
-      <p>WHY not???</p>
+      <p>Does this work ?</p>
     </host>;
   }
 }
